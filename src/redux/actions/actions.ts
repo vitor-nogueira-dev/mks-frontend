1bc6647ac56d58692408730ada413da7c1135ba7
Vitor Nogueira <vslnogueira96@gmail.com>
2023-11-13 15:58:37 -0300

    chore: creating set loading and set error actions

--- src/redux/actions/actions.ts
+++ src/redux/actions/actions.ts
@@ -1,6 +1,8 @@
 export const ADD_PRODUCT = 'ADD_PRODUCT';
 export const REMOVE_PRODUCT = 'REMOVE_PRODUCT';
 export const SET_PRODUCTS = 'SET_PRODUCTS';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ERROR = 'SET_ERROR';
 
 export const actionAddProduct = (payload = {}) => ({
   type: ADD_PRODUCT,
@@ -14,5 +16,15 @@ export const actionRemoveProduct = (payload = {}) => ({
 
 export const actionSetProducts = (payload = {}) => ({
   type: SET_PRODUCTS,
+  payload,
+});
+
+export const actionSetLoading = (payload = {}) => ({
+  type: SET_LOADING,
+  payload,
+});
+
+export const actionSetError = (payload = {}) => ({
+  type: SET_ERROR,
   payload,
 });
