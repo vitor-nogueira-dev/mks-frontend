24be9f677641535bebe64595b7d955ec08d92842
Vitor Nogueira <vslnogueira96@gmail.com>
2023-11-13 15:59:34 -0300

    chore: creating selector for state g

---
+++ src/redux/selectors/selectors.ts
@@ -0,0 +1,9 @@
+import { IPropsInitialState } from "@/interfaces/IPropsInitialState";
+
+export const getProducts = (state: IPropsInitialState) => state.products;
+
+export const getProductsCart = (state: IPropsInitialState) => state.productsCart
+
+export const getLoading = (state: IPropsInitialState) => state.loading
+
+export const getError = (state: IPropsInitialState) => state.error
