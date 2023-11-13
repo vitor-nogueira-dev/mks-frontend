49a5842902ca593f95238d3f4c00dc4c0d11d7dd
Vitor Nogueira <vslnogueira96@gmail.com>
2023-11-13 15:58:10 -0300

    chore: adding set loading and set error cart reducer

--- src/redux/reducers/cartReducer.ts
+++ src/redux/reducers/cartReducer.ts
@@ -1,10 +1,12 @@
 import { IPropsInitialState } from "@/interfaces/IPropsInitialState";
-import { ADD_PRODUCT, REMOVE_PRODUCT, SET_PRODUCTS } from "../actions/actions";
+import { ADD_PRODUCT, REMOVE_PRODUCT, SET_ERROR, SET_LOADING, SET_PRODUCTS } from "../actions/actions";
 
 
 const initialState: IPropsInitialState = {
   productsCart: {},
   products: [],
+  loading: false,
+  error: '',
 }
 
 const CartReducer = (state = initialState, { type, payload }: { type: string, payload: any }) => {
@@ -18,6 +20,21 @@ const CartReducer = (state = initialState, { type, payload }: { type: string, pa
         ...state,
       };
     case SET_PRODUCTS:
+      return {
+        ...state,
+        products: payload
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: payload
+      }
+    case SET_ERROR: {
+      return {
+        ...state,
+        error: payload
+      }
+    }
     default:
       return state;
   }
