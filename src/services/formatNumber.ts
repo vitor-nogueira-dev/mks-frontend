87475ab359fbf0315a5a82aa515535ada4a8c7c7
Vitor Nogueira <vslnogueira96@gmail.com>
2023-11-14 13:35:05 -0300

    feat: creating service format Price List Products

---
+++ src/services/formatNumber.ts
@@ -0,0 +1,9 @@
+export function formatPriceListProducts(value: number) {
+  const formattedPrice = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(value);
+  return formattedPrice;
+}
