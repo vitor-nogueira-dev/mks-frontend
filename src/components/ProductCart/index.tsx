088a567dc5224034fb54110737d3a3b1678c79a8
Vitor Nogueira <vslnogueira96@gmail.com>
2023-11-14 13:40:13 -0300

    feat: implementing and adding all component product card style

--- src/components/ProductCart/index.tsx
+++ src/components/ProductCart/index.tsx
@@ -1,25 +1,51 @@
 import React from 'react'
-import { NameProduct, PhotoProduct, PriceProduct } from '../ProductCard/styles'
-import { ContainerProductCart } from './styles'
+import { useDispatch } from 'react-redux';
 
-type Props = {}
+import { IProduct } from '@/interfaces/IProduct';
+import { ADD_PRODUCT, REMOVE_ALL_PRODUCT, REMOVE_PRODUCT } from '@/redux/actions/actions';
+import { formatPriceListProducts } from '@/services/formatNumber';
 
-function ProductCart({ }: Props) {
+import { NameProduct, PhotoProduct, PriceProduct } from '../ProductCard/styles';
+import { ContainerProductCart, CloseButton, ContentQuantity, ContentButtonsQuantity, Button } from './styles';
+
+type Props = {
+  productsCart: any;
+}
+
+function ProductCart({ productsCart }: Props) {
+
+  const dispatch = useDispatch();
+
+  const handleAddProduct = (product: IProduct) => {
+    return dispatch(({ type: ADD_PRODUCT, payload: product }))
+  }
+
+  const handleRemoveProduct = (product: IProduct) => {
+    return dispatch(({ type: REMOVE_PRODUCT, payload: product }))
+  }
+
+  const handleRemoveCompleteProduct = (product: IProduct) => {
+    return dispatch(({ type: REMOVE_ALL_PRODUCT, payload: product }))
+  }
   return (
-    <ContainerProductCart>
-      <PhotoProduct src='/icon-cart.png' width={46} height={57} />
-      <h1>ProductCart</h1>
-      <div>
-        Qtd:
-        {/* botao com a quantidade do produto adicionada ao carrinho e um button mais e outro menos para adicionar ou remover */}
-        <div>
-          <button>-</button>
-          <span>1</span>
-          <button>+</button>
-        </div>
-      </div>
-      <h4>300</h4>
-    </ContainerProductCart>
+    productsCart.map((product: any) => (
+      <ContainerProductCart key={product.id}>
+        <PhotoProduct src={product.photo} width={46} height={57} />
+        <CloseButton onClick={() => handleRemoveCompleteProduct(product)}>
+          X
+        </CloseButton>
+        <NameProduct fontSize='13px' width='100px'>{product.name}</NameProduct>
+        <ContentQuantity>
+          <small>Qtd:</small>
+          <ContentButtonsQuantity>
+            <Button onClick={() => handleRemoveProduct(product)}>-</Button>
+            <span>{product.amount}</span>
+            <Button onClick={() => handleAddProduct(product)}>+</Button>
+          </ContentButtonsQuantity>
+        </ContentQuantity>
+        <PriceProduct $width="27%" $backgroundColor="transparent" color="#000">{formatPriceListProducts(product.price)}</PriceProduct>
+      </ContainerProductCart>
+    ))
   )
 }
 
--- src/components/ProductCart/styles.ts
+++ src/components/ProductCart/styles.ts
@@ -1,7 +1,8 @@
+'use client';
 import styled from "styled-components";
 
 export const ContainerProductCart = styled.div`
-width: 379px;
+width: 379px;     
 height: 95px;
 flex-shrink: 0;
 border-radius: 8px;
@@ -11,4 +12,94 @@ display: flex;
 justify-content: space-around;
 align-items: center;
 padding: 4px;
+position: relative; 
+
+  @media (max-width: 768px) {
+    width: 338px;
+  }
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: -7px;
+  right: -6px;
+  background: #000; 
+  color: #fff; 
+  border: none;
+  cursor: pointer;
+  font-size: 14px;
+  padding: 4px 8px; 
+  border-radius: 50%; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20px; 
+  width: 20px; 
+  z-index: 10;
+  transition: background-color 0.2s, color 0.2s;
+
+  &:hover {
+    background-color: #333; 
+  }
+
+  user-select: none; 
+  -webkit-tap-highlight-color: transparent;
+`;
+
+
+
+export const ContentQuantity = styled.div`
+  small {
+    color: #000;
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+`;
+
+
+export const ContentButtonsQuantity = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: relative; 
+
+  width: 70px;
+  height: 25px;
+  flex-shrink: 0;
+
+  border-radius: 4px;
+  border: 1px solid #BFBFBF;
+  background: #FFF;
+
+  &::before, &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 1px; 
+    height: 70%; 
+    top: 50%; 
+    transform: translateY(-50%); 
+    background-color: #BFBFBF; 
+  }
+
+  &::before {
+    left: calc(33%); 
+    margin-right: -3px;
+  }
+
+  &::after {
+    right: calc(33%); 
+    margin-left: -3px;
+  }
+`;
+
+export const Button = styled.button`
+background: transparent;
+border: none;
+cursor: pointer;
+padding: 4px;
+font-size: 16px;
 `;
