b3961da64a9e23c7ba2304d5156e5055deb16699
Vitor Nogueira <vslnogueira96@gmail.com>
2023-11-14 13:44:43 -0300

    chore: including css toastify, font montserrat and theme provider in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 'use client';
-import { Montserrat } from 'next/font/google'
 import './globals.css'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import store from '@/redux/store';
+import { Montserrat } from 'next/font/google'
+import { ThemeProvider } from "@material-tailwind/react";
+import 'react-toastify/dist/ReactToastify.css';
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -23,11 +25,13 @@ export default function RootLayout({
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </head>
       <body className={montserrat.className}>
-        <Provider store={store}>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
-        </Provider>
+        <ThemeProvider>
+          <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </Provider>
+        </ThemeProvider>
       </body>
     </html>
   )
